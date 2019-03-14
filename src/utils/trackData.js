67d658b03858a95d172c67dcb89bc5c761ba67e1
naffiq <abdu.galymzhan@gmail.com>
2019-03-14 20:41:04 +0100

    Shazam data script included

--- src/utils/trackData.js
+++ src/utils/trackData.js
@@ -1,9 +1,35 @@
+import { shazamData } from "../data/shazamData";
+
 export const flattenAlbums = albumData =>
   albumData.reduce(
     (allTracksData, album, albumIdx) =>
       allTracksData.concat(album.tracks.map(track => ({ ...track, albumIdx }))),
     []
   );
+
+export const addShazams = albums => {
+  const albumsWithShazams = albums.map(album => {
+    const tracksWithShazams = album.tracks
+      .map(track => {
+        const shazamLink = shazamData.find(shazamTrack => {
+          return shazamTrack.Title.toLowerCase() === track.name.toLowerCase();
+        });
+        const shazams = shazamLink ? shazamLink["Shazams per year"] : "0";
+
+        return {
+          ...track,
+          shazams
+        };
+      })
+      .filter(track => track.shazams > 0);
+    return {
+      ...album,
+      tracks: tracksWithShazams
+    };
+  });
+
+  return albumsWithShazams;
+};
 
 export const getMaxValue = (albumData, trackField) =>
   albumData.reduce((currentMax, album) => {
